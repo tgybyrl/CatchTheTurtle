8c5916e7cfc7ac0fee837175c4c458cc39164a04
Tugay Bayırlı <tgybyrl48@gmail.com>
2024-11-02 16:37:16 +0200

    Screen and turtle created

---
+++ turtleGame.py
@@ -0,0 +1,16 @@
+import turtle
+from random import randint
+
+game_screen = turtle.Screen()
+game_screen.title("Catch The Turtle!")
+game_screen.bgcolor("light gray")
+
+
+turta = turtle.Turtle()
+turta.shape('turtle')
+turta.color("green")
+turta.shapesize(2)
+turta.speed(3)
+
+
+turtle.mainloop()
